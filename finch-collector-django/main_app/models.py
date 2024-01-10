8a1163fe52f245312dd7862dbd51f262aa8f9083
isbrakha <isaacbrakha14@gmail.com>
2024-01-10 13:01:30 -0500

    part 3 complete

--- finch-collector-django/main_app/models.py
+++ finch-collector-django/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    favorite_music_genre = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+    
+    
--- finch-collector-django/main_app/views.py
+++ finch-collector-django/main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 
-
-finches = [
-       {'name': 'Marcus Aurelius', 'breed': 'Golden Finchtreiver', 'description': 'Smart and cute', 'age': 3},
-       {'name': 'Jennifer', 'breed': 'Evening grosebeak', 'description': 'Likes to fly', 'age': 2},
-       {'name': 'Lewis', 'breed': 'Purple Finch', 'description': 'Is red', 'age': 4},
+# finches = [
+#        {'name': 'Marcus Aurelius', 'breed': 'Golden Finchtreiver', 'description': 'Smart and cute', 'age': 3},
+#        {'name': 'Jennifer', 'breed': 'Evening grosebeak', 'description': 'Likes to fly', 'age': 2},
+#        {'name': 'Lewis', 'breed': 'Purple Finch', 'description': 'Is red', 'age': 4},
        
-]
+# ]
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -15,6 +16,24 @@ def about(request):
           return render(request, 'about.html')
 
 def finches_index(request):
+       finches = Finch.objects.all()
        return render(request, 'finches/index.html', {
               'finches' : finches
        })
+
+def finches_detail(request, finch_id):
+       finch = Finch.objects.get(id=finch_id)
+       return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+       model = Finch
+       fields = '__all__'
+
+class FinchUpdate(UpdateView):
+       model = Finch
+       # Let's disallow the renaming of a cat by excluding the name field!
+       fields = ['breed', 'description', 'favorite_music_genre']
+
+class FinchDelete(DeleteView):
+       model = Finch
+       success_url = '/finches'
